061011524b6dfa2207627232e9581e83933130d4
Kunal <kunal20vijay00@gmail.com>
2022-06-04 05:13:33 +0530

    password hashing using bcryptjs and generating authToken using jwtwebtoken

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Have$omeNote$";
 
 // Create a user using : POST "api/auth/createUser".Doesn't require authentication
 router.post(
@@ -28,14 +32,24 @@ router.post(
           .status(400)
           .json({ error: "Email has already been taken. Use another email." });
       }
+
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       // Creating new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
-      
+
+      const data = {
+        user:{
+          id:user.id  //since id is unique
+        }
+      }
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({authToken});
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Some error occurred");
