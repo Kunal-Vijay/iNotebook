5eefce4e1fba361ee70363dd276161905d3aae1b
Kunal <kunal20vijay00@gmail.com>
2022-06-04 19:46:43 +0530

    created login endpoint

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -43,18 +43,62 @@ router.post(
       });
 
       const data = {
-        user:{
-          id:user.id  //since id is unique
-        }
-      }
+        user: {
+          id: user.id, //since id is unique
+        },
+      };
 
       const authToken = jwt.sign(data, JWT_SECRET);
-      res.json({authToken});
+      res.json({ authToken });
     } catch (err) {
       console.error(err.message);
-      res.status(500).send("Some error occurred");
+      res.status(500).send("Internal Server Error");
     }
   }
 );
 
+// Authenticate a user using: POST "/api/auth/login"
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(), // email must be an email
+    body("password", "Password cannot be blank").exists(), // password should be minimum of 8 chars
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({
+          error:
+            "Incorrect Username or Password : Please try to login with credentials",
+        });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({
+          error:
+            "Incorrect Username or Password : Please try to login with credentials",
+        });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.log({ error: error.message });
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
 module.exports = router;
