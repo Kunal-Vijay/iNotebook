6a04bd1aa0e1d7b01599f03293bbd0dd15ef1b97
Kunal <kunal20vijay00@gmail.com>
2022-06-23 10:20:35 +0530

    endpoints for adding and fetching notes

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -2,10 +2,14 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const notesSchema = new Schema({
-    name : {type:String,required:true},
-    description : {type:String,required:true},
-    tag : {type:String,required:true},
-    date : {type:Date,default:Date.now},
+    user: {
+        type: mongoose.Schema.Types.ObjectId,     // like foreign key in mysql
+        ref: 'user'                               // reference model 
+    },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    tag: { type: String, required: true },
+    date: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('notes',notesSchema);
+module.exports = mongoose.model('notes', notesSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const fetchuser = require("../middleware/fetchusers");
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  res.json([]);
-});
+// ROUTE 1 : Fetch notes using : GET "api/notes/fetchallnotes" . Login required
+router.get('/fetchallnotes', fetchuser,
+
+  async (req, res) => {
+    try {
+      const notes = await Notes.find({ user: req.user.id });
+      res.json(notes);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+// ROUTE 2 : Add new notes using : POST "api/notes/addnotes". Login required
+router.post('/addnotes', fetchuser,
+  [
+    body("title", "Title must contain minimum 3 characters").isLength({ min: 3 }),
+    body("description", "Description must contain minimum 5 characters").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Notes({
+        title, description, tag, user: req.user.id
+      })
+      const savedNote = await note.save();
+      res.json(savedNote);
+
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
 
 module.exports = router;
