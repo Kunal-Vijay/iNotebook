a27708d228c0fb8ec0aa71009ae2c11844793b75
Kunal <kunal20vijay00@gmail.com>
2022-06-23 11:00:22 +0530

    added endpoint for updating note

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -17,8 +17,8 @@ router.get('/fetchallnotes', fetchuser,
     }
   });
 
-// ROUTE 2 : Add new notes using : POST "api/notes/addnotes". Login required
-router.post('/addnotes', fetchuser,
+// ROUTE 2 : Add new notes using : POST "api/notes/addnote". Login required
+router.post('/addnote', fetchuser,
   [
     body("title", "Title must contain minimum 3 characters").isLength({ min: 3 }),
     body("description", "Description must contain minimum 5 characters").isLength({ min: 5 }),
@@ -36,6 +36,30 @@ router.post('/addnotes', fetchuser,
       const savedNote = await note.save();
       res.json(savedNote);
 
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+// ROUTE 3 : Update notes using : PUT "api/notes/updatenote". Login required
+router.put('/updatenote/:id', fetchuser,
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      // creating a new note object
+      const newNote = {};
+      if (title) { newNote.title = title }
+      if (description) { newNote.description = description }
+      if (tag) { newNote.tag = tag }
+
+      // Find the note to be updated and update it 
+      let note = await Notes.findById(req.params.id);
+      if (!note) { return res.status(404).send("Note not found") }
+      if (note.user.toString() !== req.user.id) { return res.status(401).send("Unauthorize access") }
+
+      note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+      res.json(note);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal Server Error");
