f4a60bddf6de8af4f9ea42cdfd23c62efb35b0d6
Kunal <kunal20vijay00@gmail.com>
2022-06-24 11:57:10 +0530

    adding notes to website

--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NoteContext from './noteContext';
 
-const NoteState = (props) => { 
+const NoteState = (props) => {
+    const initialNotes = [
+        {
+            "_id": "62b3e4a0d6165a77a019c641",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:57:20.050Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b3e4a0d6165a77a019c641",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:57:20.050Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b3e4a0d6165a77a019c641",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:57:20.050Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b3e4a0d6165a77a019c641",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:57:20.050Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b3e4a0d6165a77a019c641",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:57:20.050Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b3e4eccb0a608e953ad8df",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:58:36.176Z",
+            "__v": 0
+        },
+        {
+            "_id": "62b3e50775f37cbb3e95ba57",
+            "user": "62a8a8706e926b665d44cc53",
+            "title": "My notes",
+            "description": "This is my first note",
+            "tag": "personal",
+            "date": "2022-06-23T03:59:03.390Z",
+            "__v": 0
+        }
+    ]
+    const [notes, setNotes] = useState(initialNotes)
     return (
-        <NoteContext.Provider >
+        <NoteContext.Provider value={{notes,setNotes}} >
             {props.children}
         </NoteContext.Provider>
     )
